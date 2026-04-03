c66da8a9e59ade5866c2c2bd18d63b71de065c11
BENwada <domo0103wada@gmail.com>
2026-04-03 22:45:11 +0900

    wadake config

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,42 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    local hl_groups = {
+      "Normal",
+      -- "NormalFloat",
+      -- "FloatBorder",
+      "SignColumn",
+      "EndOfBuffer",
+      "LineNr",
+    }
+    for _, group in ipairs(hl_groups) do
+      vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" })
+    end
+  end,
+})
+
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   -- 対象拡張子を限定
+--   pattern = { "*.ts", "*.tsx", "*.js", "*.jsx", "*.json", "*.jsonc" },
+--   callback = function()
+--     -- 現在のバッファで biome が動いているか確認
+--     local clients = vim.lsp.get_clients({ name = "biome", bufnr = 0 })
+--     if #clients > 0 then
+--       vim.lsp.buf.code_action({
+--         context = {
+--           diagnostics = vim.diagnostic.get(0), -- 必須フィールド
+--           only = {
+--             -- ここを必ず「大文字」を含めた正しい形式にする
+--             "source.fixAll.biome",
+--             "source.organizeImports.biome",
+--             "quickfix.biome.useSortedClasses",
+--           },
+--         },
+--         apply = true,
+--       })
+--     end
+--   end,
+-- })
