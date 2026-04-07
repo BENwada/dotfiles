7368411f12ed210287e2058c02d7c2a3f2236b87
BENwada <domo0103wada@gmail.com>
2026-04-07 14:55:42 +0900

    colorizer install
    "NvChad/nvim-colorizer.lua",
      opts = {
        user_default_options = {
          -- Tailwind CSS v4のOKLCHカラーなどの可視化を有効にする
          oklch = true,
          --
    Tailwindのユーティリティクラス（text-blue-500など）のプレビューを有効にする
          tailwind = true,
          -- その他の一般的な設定
          css = true,
          css_fn = true, -- CSS関数の可視化
        },
      },
    }

---
+++ nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,14 @@
+return {
+  "NvChad/nvim-colorizer.lua",
+  opts = {
+    user_default_options = {
+      -- Tailwind CSS v4のOKLCHカラーなどの可視化を有効にする
+      oklch = true,
+      -- Tailwindのユーティリティクラス（text-blue-500など）のプレビューを有効にする
+      tailwind = true,
+      -- その他の一般的な設定
+      css = true,
+      css_fn = true, -- CSS関数の可視化
+    },
+  },
+}
